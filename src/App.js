210a154c78591da0d25bdbc8e0f0e86cf9747493
Davis Nguyen <davisnguyen@Daviss-MacBook-Air.local>
2022-10-26 13:25:12 -0500

    article, clothes, fittingroom components created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import FittingRoom from './components/FittingRoom'
+import './index.css'
+
 function App() {
   return (
     <div>
       <header>VirtuFit</header>
+      <FittingRoom />
     </div>
   )
 }
---
+++ src/components/Clothes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Article from './Article'
+
+export default function Clothes() {
+  return (
+    <>
+      <div className='x'>
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+      </div>
+    </>
+  )
+}
